3f866ea55e1c32d527f162f87b945494bf908875
rostislav <rostislav.plotnikov.04@mail.ru>
2022-06-23 15:43:06 +0300

    fix class figure

--- chess/src/models/figure/King.ts
+++ chess/src/models/figure/King.ts
@@ -1,7 +1,15 @@
-
-
-import { Figure } from "./Figure";
+import { Cell } from "../Cell";
+import { Colors } from "../Colors";
+import { Figure, FigureNames } from "./Figure";
+import blackLogo from "../../assets/black-king.png";
+import whiteLogo from "../../assets/black-king.png";
 
 export class King extends Figure {
+
+    constructor(color: Colors, cell: Cell) {
+        super(color, cell);
+        this.logo = color === Colors.BLACK ? blackLogo : whiteLogo ;
+        this.name = FigureNames.KING;
+    }
 
 }
