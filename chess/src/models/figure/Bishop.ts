516f3ed05a49855bce63dc31df3657fc5e69cc74
rostislav <rostislav.plotnikov.04@mail.ru>
2022-06-23 20:46:05 +0300

    add move bishop

--- chess/src/models/figure/Bishop.ts
+++ chess/src/models/figure/Bishop.ts
@@ -16,9 +16,10 @@ export class Bishop extends Figure {
     canMove(target: Cell): boolean {
         if (!super.canMove(target)) {
             return false;
-        } else {
+        }
+        if(this.cell.isEmptyDiagonal(target)) {
             return true;
         }
+        return false;
     }
-
 }
