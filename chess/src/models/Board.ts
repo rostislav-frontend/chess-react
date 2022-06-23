d02191e1cd7e23bbb7d6992012a260c99ec65fe8
rostislav <rostislav.plotnikov.04@mail.ru>
2022-06-23 16:02:28 +0300

    arranged the figures and fix import king class

--- chess/src/models/Board.ts
+++ chess/src/models/Board.ts
@@ -1,6 +1,11 @@
 import { Cell } from "./Cell";
 import { Colors } from "./Colors";
+import { Bishop } from "./figure/Bishop";
+import { King } from "./figure/King";
+import { Knight } from "./figure/Knight";
+import { Pawn } from "./figure/Pawn";
 import { Queen } from "./figure/Queen";
+import { Rook } from "./figure/Rook";
 
 export class Board {
     cells: Cell[][] = [];
@@ -29,9 +34,54 @@ export class Board {
         return this.cells[y][x]
     }
 
-    public addFigures() {
-        new Queen(Colors.WHITE, this.getCell( 3, 3))
+    private addPawns() {
+        for (let i = 0; i < 8; i++) {
+            new Pawn(Colors.BLACK, this.getCell(i, 1));
+            new Pawn(Colors.WHITE, this.getCell(i, 6));
+            
+        }
+    }
 
+    private addKings() {
+        new King(Colors.BLACK, this.getCell(4, 0));
+        new King(Colors.WHITE, this.getCell(4, 7));
+
+    }
+
+    private addQueens() {
+        new Queen(Colors.BLACK, this.getCell(3, 0));
+        new Queen(Colors.WHITE, this.getCell(3, 7));
+    }
+
+    private addBioshops() {
+        new Bishop(Colors.BLACK, this.getCell(2, 0));
+        new Bishop(Colors.BLACK, this.getCell(5, 0));
+        new Bishop(Colors.WHITE, this.getCell(2, 7));
+        new Bishop(Colors.WHITE, this.getCell(5, 7));
+    }
+
+    private addKnights() {
+        new Knight(Colors.BLACK, this.getCell(1, 0));
+        new Knight(Colors.BLACK, this.getCell(6, 0));
+        new Knight(Colors.WHITE, this.getCell(1, 7));
+        new Knight(Colors.WHITE, this.getCell(6, 7));
+    }
+
+    private addRooks() {
+        new Rook(Colors.BLACK, this.getCell(0, 0));
+        new Rook(Colors.BLACK, this.getCell(7, 0));
+        new Rook(Colors.WHITE, this.getCell(0, 7));
+        new Rook(Colors.WHITE, this.getCell(7, 7));
+    }
+
+    public addFigures() {
+        this.addPawns();
+        this.addKings();
+        this.addQueens();
+        this.addBioshops();
+        this.addBioshops();
+        this.addKnights();
+        this.addRooks();
     }
 
 }
--- chess/src/models/figure/King.ts
+++ chess/src/models/figure/King.ts
@@ -2,7 +2,7 @@ import { Cell } from "../Cell";
 import { Colors } from "../Colors";
 import { Figure, FigureNames } from "./Figure";
 import blackLogo from "../../assets/black-king.png";
-import whiteLogo from "../../assets/black-king.png";
+import whiteLogo from "../../assets/white-king.png";
 
 export class King extends Figure {
 
