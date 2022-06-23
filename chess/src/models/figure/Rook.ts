0df7192b11254b6e1dd758024b4a2b452b0d290b
rostislav <rostislav.plotnikov.04@mail.ru>
2022-06-23 20:44:12 +0300

    fix rook move

--- chess/src/models/figure/Rook.ts
+++ chess/src/models/figure/Rook.ts
@@ -15,8 +15,13 @@ export class Rook extends Figure {
     canMove(target: Cell): boolean {
         if (!super.canMove(target)) {
             return false;
-        } else {
+        } 
+        if(this.cell.isEmptyVertical(target)) {
             return true;
         }
+        if(this.cell.isEmptyHorizontal(target)) {
+            return true;
+        }
+        return false;
     }
 }
